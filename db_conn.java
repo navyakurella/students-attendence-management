df6bbd4f2b90d526708753ce15984f171bde1197
navyakurella <119152167+navyakurella@users.noreply.github.com>
2023-09-07 22:50:48 +0530

    Update db_conn.java

--- db_conn.java
+++ db_conn.java
@@ -16,7 +16,7 @@ System.out.println("records inserted");
 int n=stmt.executeUpdate("delete from ITSTUDENT where id=1201");
 System.out.println(n+" records affected");
 n=stmt.executeUpdate("update ITSTUDENT set name='Ahmed',address='VIZAG' where id=1202");    
-System.out.println(n+" records affected");
+System.out.println(n+" records affected ");
 ResultSet rs=stmt.executeQuery("select * from itstudent");  
 while(rs.next()){  
 System.out.println(rs.getString(1)+" "+rs.getInt(2)+" "+rs.getString(3));  
